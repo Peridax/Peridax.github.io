f15667b8e7e7f244e9c1fffa0f556b66c1420bb3
Peridax <evandrojr902@gmail.com>
2021-04-07 14:42:22 -0400

    Added contact form, tweaked styling
    - Added a fully functional contact form
    - Further styled the page
    - Finalized theme colors
    - Added toasts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,80 @@
 $(() => {
   const myFullpage = new fullpage('#scroller', {
       /* Options */
-      sectionsColor: ['#f39c12', '#8e44ad', '#7BAABE', '#2980b9'],
+      sectionsColor: ['#f39c12', '#ffffff', '#ffffff', '#ffffff'],
       navigationTooltips: ['Home', 'Skills', 'Portfolio', 'Contact'],
       anchors: ['home', 'skills', 'portfolio', 'contact'],
 
       dragAndMove: true,
       loopBottom: true,
       navigation: true,
-	    showActiveTooltip: false
+      slidesNavigation: true,
+      slidesNavPosition: 'bottom',
+	    showActiveTooltip: false,
+
+      controlArrows: true
   });
+
+  const desired = "Full Stack Developer"
+  let index = 0
+
+  const skillLoop = () => {
+    setTimeout(() => {
+      $('#skill-text').text(desired.substring(0, index + 1))
+      index++
+
+      if (index < desired.length) {
+        skillLoop()
+      }
+    }, 125)
+  }
+
+  skillLoop()
+
+  $('.custom-prev').click(() => {
+    fullpage_api.moveSlideLeft();
+  })
+
+  $('.custom-next').click(() => {
+    fullpage_api.moveSlideRight();
+  })
+
+  /* Handling contact form submission */
+  const form = document.getElementById('contact-form')
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    const data = new FormData(event.target)
+
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      const newToast = `
+      <div class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+          <div class="toast-body">
+            Thanks for your submission!
+          </div>
+          <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+      </div>
+      `
+      $('.toast-container').html(newToast)
+      $('.toast').toast('show')
+      form.reset()
+    })
+    .catch(error => {
+      alert('Did not send!')
+    })
+  }
+
+  form.addEventListener("submit", handleSubmit)
+
+  $('#myToast').toast('show')
 })
