019a31280ca75664d14ab1789e4d47ef0066a2b3
Peridax <evandrojr902@gmail.com>
2021-04-10 20:47:25 -0400

    Added meta tags, optimized javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
+// Setting global variables
+let form
+
 $(() => {
+  // Initializing Fullpage.js
   const myFullpage = new fullpage('#scroller', {
       /* Options */
       sectionsColor: ['#f39c12', '#ffffff', '#ffffff', '#ffffff'],
@@ -16,22 +20,10 @@ $(() => {
       controlArrows: false
   });
 
-  const desired = "Full Stack Developer"
-  let index = 0
+  // Initializing Text Typer On Homepage
+  skillContainer('Full Stack Developer')
 
-  const skillLoop = () => {
-    setTimeout(() => {
-      $('#skill-text').text(desired.substring(0, index + 1))
-      index++
-
-      if (index < desired.length) {
-        skillLoop()
-      }
-    }, 125)
-  }
-
-  skillLoop()
-
+  // Managing click events for slider buttons
   $('.prev').click(() => {
     fullpage_api.moveSlideLeft();
   })
@@ -41,41 +33,55 @@ $(() => {
   })
 
   /* Handling contact form submission */
-  const form = document.getElementById('contact-form')
+  form = document.getElementById('contact-form')
+  form.addEventListener("submit", handleSubmit)
+})
 
-  async function handleSubmit(event) {
-    event.preventDefault()
+const skillContainer = text => {
+  let index = 0
 
-    const data = new FormData(event.target)
+  const skillLoop = () => {
+    setTimeout(() => {
+      $('#skill-text').text(text.substring(0, index + 1))
+      index++
 
-    fetch(event.target.action, {
-      method: form.method,
-      body: data,
-      headers: {
-        'Accept': 'application/json'
+      if (index < text.length) {
+        skillLoop()
       }
-    })
-    .then(response => {
-      const newToast = `
-      <div class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="d-flex">
-          <div class="toast-body">
-            Thanks for your submission!
-          </div>
-          <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-      </div>
-      `
-      $('.toast-container').html(newToast)
-      $('.toast').toast('show')
-      form.reset()
-    })
-    .catch(error => {
-      alert('Did not send!')
-    })
+    }, 125)
   }
 
-  form.addEventListener("submit", handleSubmit)
+  skillLoop()
+}
 
-  $('#myToast').toast('show')
-})
+const handleSubmit = async event => {
+  event.preventDefault()
+
+  const data = new FormData(event.target)
+
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .then(response => {
+    const newToast = `
+    <div class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="d-flex">
+        <div class="toast-body">
+          Thanks for your submission!
+        </div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+    </div>
+    `
+    $('.toast-container').html(newToast)
+    $('.toast').toast('show')
+    form.reset()
+  })
+  .catch(error => {
+    alert('Did not send!')
+  })
+}
